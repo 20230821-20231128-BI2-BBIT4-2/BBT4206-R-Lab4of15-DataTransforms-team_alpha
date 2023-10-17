18f741f0d1311ba8ea606fba1635ff1924753fef
balbert103 <94045393+balbert103@users.noreply.github.com>
2023-10-17 10:36:43 +0300

    modeling

---
+++ Lab4-Submission-ExposingTheStructureOfDataUsingDataTransforms.R
@@ -0,0 +1,6 @@
+if (require("languageserver")) {
+  require("languageserver")
+} else {
+  install.packages("languageserver", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
